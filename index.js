504f2c71c0114753266635ab4768609281847a77
Dhairya Dutt <dhairyadutt2002@gmail.com>
2021-08-09 19:52:52 +0530

    Inserted a movie in the db through mongoose

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, u
     console.log("Error!!!!");
     console.log(err);
 });
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+const suicideSquad = new Movie({title: 'Suicide Squad', year: 2021, score:9,rating:'R'})
